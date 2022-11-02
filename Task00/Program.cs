7d13500c0205267442bf7e02cb867921b8e6dc52
Nikolay <nikornetov@mail.ru>
2022-11-02 21:14:40 +0300

    Add Block Diagram

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -2,3 +2,20 @@
 // длина которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] specifiedArray = new string[] { "1234", "C#", "76543", "teftel", "cat", "git", "gb" };
+Console.WriteLine("Заданный массив: ");
+PrintArray(specifiedArray);
+int lengthCharacters = 3;
+
+// Метод печати массива.
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
