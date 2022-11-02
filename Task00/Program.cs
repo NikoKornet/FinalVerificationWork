635687032b750a7c6cdbff840525eb476dbdd24f
Nikolay <nikornetov@mail.ru>
2022-11-02 21:25:54 +0300

    Add the method of filling the formed masive

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -7,7 +7,12 @@ string[] specifiedArray = new string[] { "1234", "C#", "76543", "teftel", "cat",
 Console.WriteLine("Заданный массив: ");
 PrintArray(specifiedArray);
 int lengthCharacters = 3;
+
 int lengthSecondaryArr = DetermineLengthSecondaryArr(specifiedArray, lengthCharacters);
+string[] secondaryArray = new string[lengthSecondaryArr];
+FillingSecondaryArr(specifiedArray, secondaryArray, lengthCharacters);
+Console.WriteLine("Сформированный массив: ");
+PrintArray(secondaryArray);
 
 // Найдем длину будущего массива.
 int DetermineLengthSecondaryArr(string[] arr, int lengthChar)
@@ -18,6 +23,20 @@ int DetermineLengthSecondaryArr(string[] arr, int lengthChar)
         if (arr[i].Length <= lengthChar) counter++;
     }
     return counter;
+}
+
+// Наполнение сформированного массива данными.
+void FillingSecondaryArr(string[] primaryArr, string[] secondaryArr, int lenSecondArr)
+{
+    int arrIndex = 0;
+    for (int i = 0; i <= primaryArr.Length - 1; i++)
+    {
+        if (primaryArr[i].Length <= lenSecondArr)
+        {
+            secondaryArr[arrIndex] = primaryArr[i];
+            arrIndex++;
+        }
+    }
 }
 
 // Метод печати массива.
